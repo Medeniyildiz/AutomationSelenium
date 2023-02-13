6ade8610d75d99eef1ec44e005d8ff2d69a639d9
Medeni Rostem <mrostem04@gmail.com>
2023-02-13 10:32:45 -0600

    update code

---
+++ src/test/java/Pages/AlertPracticePage.java
@@ -0,0 +1,25 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AlertPracticePage {
+//1. Go to website: http://practice.cydeo.com/javascript_alerts
+    //2. Click for JS Prompt button for alert to be displayed
+    //3. Write "hello" in input text
+    //3. Click OK button from the alert
+    //4. Verify "You entered: hello" text is displayed in result
+
+    public AlertPracticePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button[@onclick='jsPrompt()']")
+    public WebElement jsAlertBtn;
+
+    @FindBy(id="result")
+    public WebElement resultText;
+
+}
